10b1b00c658152579171f7c3f35e933eb13000b5
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2019-12-02 13:51:42 +0530

    Adding Lab4 to the exercises.

---
+++ Lab4/app.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"log"
+)
+
+type App struct {
+	Router *mux.Router
+	DB *sql.DB
+}
+
+func (a *App) Initialize(user, password, dbname string) {
+	connectionString := fmt.Sprintf("%s:%s@/%s", user, password, dbname)
+	var err error
+	a.DB, err = sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	a.Router = mux.NewRouter()
+}
+
+func (a *App) Run (addr string) {}
